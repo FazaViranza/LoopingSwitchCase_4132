058c15a0dfef4c9fa75c393c02f93ddc14ac181e
FazaViranza <nofila.faza.ft24@mail.umy.ac.id>
2025-03-18 20:41:29 +0700

    Do While

---
+++ Task1Week4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Pencacah Naik
+    int i;
+    for (i = 0; i < 5; i++){
+        cout << "Nilai i = "<< i <<" Lari Keliling Lapangan" << endl; 
+    }
+    cout << "Nilai i terakhir : " << i << endl;
+
+    //Pencaacah turun
+    for (i = 5; i > 0; i--){
+        cout << "Nilai 1 = "<< i <<"Lari Keliling Lapangan" << endl;
+    }
+    cout << 'Nilai i terakhir : ' << i << endl;
+}
+ 
